82f7a1996a68c20387b663b4e776bf281bb5643a
Konrad <kondykox@gmail.com>
2024-02-24 21:16:47 +0100

    added og skin

--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,65 +1,76 @@
 import skinsData from "../../data/skins.json";
 
-// Generate skins on page
+let areSkinsRendered = false;
+
+const skinsContainer = document.querySelector(".skins");
+const playerSkinKey = "playerSkin";
+let coins = parseInt(localStorage.getItem("coins")) || 0;
+
+// Generate skin
+function createSkinElement(skin) {
+  const skinElement = document.createElement("div");
+  skinElement.classList.add("skin");
+
+  const imgElement = document.createElement("img");
+  imgElement.src = skin.imgSrc;
+  imgElement.alt = `${skin.name} skin`;
+  imgElement.classList.add("skin__img");
+
+  const descriptionElement = document.createElement("div");
+  descriptionElement.classList.add("skin__description");
+
+  const nameElement = document.createElement("span");
+  nameElement.classList.add("skin__name");
+  nameElement.textContent = skin.name;
+
+  const priceElement = document.createElement("span");
+  priceElement.classList.add("skin__price");
+  priceElement.innerHTML = `Price: <span class="price-value">${skin.price}</span> <img src="./img/boosts/coin.png" alt="Skin price" />`;
+
+  descriptionElement.appendChild(nameElement);
+  descriptionElement.appendChild(priceElement);
+  skinElement.appendChild(imgElement);
+  skinElement.appendChild(descriptionElement);
+
+  return skinElement;
+}
+
+// Buy skin
+function buySkin(skinElement) {
+  const imgElement = skinElement.querySelector("img");
+  const nameElement = skinElement.querySelector(".skin__name");
+  const priceElement = skinElement.querySelector(".price-value");
+
+  imgElement.addEventListener("click", () => {
+    const skinName = nameElement.textContent.trim().toLowerCase();
+    const skinPrice = parseInt(priceElement.textContent.trim());
+
+    const confirmation = window.confirm(
+      `Are you sure you want to buy: ${skinName}\nPrice: ${skinPrice}`
+    );
+
+    if (confirmation) {
+      if (coins >= skinPrice) {
+        coins -= skinPrice;
+        localStorage.setItem(playerSkinKey, skinName);
+        localStorage.setItem("coins", coins);
+        alert("Skin purchased!");
+      } else {
+        alert("You don't have enough coins.");
+      }
+    }
+  });
+}
+
+// Render skins list
 export default function renderSkins() {
-  const skinsContainer = document.querySelector(".skins");
+  if (areSkinsRendered) return;
 
   skinsData.forEach((skin) => {
-    const skinElement = document.createElement("div");
-    skinElement.classList.add("skin");
-
-    const imgElement = document.createElement("img");
-    imgElement.src = skin.imgSrc;
-    imgElement.alt = `${skin.name} skin`;
-    imgElement.classList.add("skin__img");
-
-    const descriptionElement = document.createElement("div");
-    descriptionElement.classList.add("skin__description");
-
-    const nameElement = document.createElement("span");
-    nameElement.classList.add("skin__name");
-    nameElement.textContent = skin.name;
-
-    const priceElement = document.createElement("span");
-    priceElement.classList.add("skin__price");
-
-    priceElement.innerHTML = `Price: <span class="price-value">${skin.price}</span> <img src="./img/boosts/coin.png" alt="Skin price" />`;
-
-    descriptionElement.appendChild(nameElement);
-    descriptionElement.appendChild(priceElement);
-    skinElement.appendChild(imgElement);
-    skinElement.appendChild(descriptionElement);
+    const skinElement = createSkinElement(skin);
     skinsContainer.appendChild(skinElement);
+    buySkin(skinElement);
   });
 
-  // Buy skins
-  const skinDivs = document.querySelectorAll(".skin");
-
-  // Get coins from localStorage
-  let coins = localStorage.getItem("coins");
-
-  skinDivs.forEach((skinDiv) => {
-    const imgElement = skinDiv.querySelector("img");
-
-    const nameElement = skinDiv.querySelector(".skin__name");
-    const priceElement = skinDiv.querySelector(".price-value");
-
-    imgElement.addEventListener("click", () => {
-      const skinName = nameElement.textContent.trim().toLowerCase();
-      const skinPrice = priceElement.textContent.trim();
-
-      const confirmation = window.confirm(
-        `Are you sure you want to buy: ${skinName}\nPrice: ${skinPrice}`
-      );
-
-      if (confirmation) {
-        if (coins >= skinPrice) {
-          coins -= parseInt(skinPrice);
-          localStorage.setItem("playerSkin", skinName);
-
-          alert("Skin purchased!");
-        } else alert("You don't have money XD");
-      } else alert("You don't want skins :(");
-    });
-  });
+  areSkinsRendered = true;
 }
